bc7baccccdc9f35bab3331bc7b73209d7cb58148
Jacob Eiderman <jacobeid@gmail.com>
2021-11-22 18:15:51 +0200

    Implement IPv4 handle method

--- ipv4.py
+++ ipv4.py
@@ -1,5 +1,7 @@
 from typing import Optional, Tuple, Type
 import struct
+from io import BytesIO
+import ipaddress
 
 from stack import NetworkAdapterInterface, ProtocolInterface
 from protocol import Protocol
@@ -26,6 +28,14 @@ class IPv4(Protocol):
         return '.'.join(str(part) for part in ip)
 
     @staticmethod
+    def encode_ip(ip: str) -> int:
+        return int(ipaddress.IPv4Address(ip))
+
+    @staticmethod
+    def decode_ip(ip: int) -> str:
+        return str(ipaddress.IPv4Address(ip))
+
+    @staticmethod
     def calculate_checksum(header: bytes) -> bytes:
         s = 0
         for i in range(0, len(header), 2):
@@ -38,13 +48,33 @@ class IPv4(Protocol):
     async def build(self, adapter: NetworkAdapterInterface, packet: bytes, options) -> bytes:
         ip_header = self.PROTOCOL_STRUCT.pack(
             (self.VERSION << 4) + self.HEADER_LENGTH, 0, len(packet) + self.HEADER_LENGTH * 4, 
-            0, 0, self.TTL, options['protocol'])
-        ip_header += struct.pack('H', IPv4.calculate_checksum(ip_header))
-        ip_header += IPv4.build_ip(adapter.ip)
-        ip_header += IPv4.build_ip(options['dst_ip'])
+            0, 0, self.TTL, options['protocol'], 0, IPv4.encode_ip(adapter.ip), IPv4.encode_ip(options['dst_ip']))
+        checksum = IPv4.calculate_checksum(ip_header)
+        ip_header = ip_header[:10] + struct.pack('>H', checksum) + ip_header[12:]  # insert real checksum
         return ip_header + packet
 
     async def handle(self, packet: bytes, adapter: NetworkAdapterInterface, packet_description: dict) \
             -> Optional[Tuple[bytes, int]]:
-        # TODO
-        pass
+        packet_io = BytesIO(packet)
+        header_data = packet_io.read(self.PROTOCOL_STRUCT.size)
+        version_and_header_length, options, total_length, identification, flags_and_fragment_offset, ttl, protocol, header_checksum, src_ip, dst_ip = self.PROTOCOL_STRUCT.unpack(header_data)
+
+        calculated_checksum = IPv4.calculate_checksum(header_data[:10] + b'\x00' * 2 + header_data[12:])
+        assert header_checksum == calculated_checksum, "received checksum doesn't match calculated checksum"
+
+        if version_and_header_length != 0x45 \
+                or options != 0 \
+                or flags_and_fragment_offset != 0:
+            return None
+
+        src_ip = IPv4.decode_ip(src_ip)
+        dst_ip = IPv4.decode_ip(dst_ip)
+
+        if dst_ip != adapter.ip:
+            return None
+
+        packet_description['src_ip'] = src_ip
+        packet_description['dst_ip'] = dst_ip
+
+        payload = packet_io.read(total_length - 20)
+        return payload, protocol
--- test/test_ipv4.py
+++ test/test_ipv4.py
@@ -10,6 +10,8 @@ import consts
 
 TEST_DST_IP = '1.1.1.1'
 TEST_DST_MAC = 'aa:aa:aa:aa:aa:aa'
+TEST_PREVIOUS_ID = 123
+TEST_PAYLOAD = b'abcde'
 
 def assert_packet(packet: bytes, adapter: MockNetworkAdapter):
     packet = Ether(packet)
@@ -23,7 +25,30 @@ def assert_packet(packet: bytes, adapter: MockNetworkAdapter):
     assert ip.src == adapter.ip
     assert ip.dst == TEST_DST_IP
 
+def assert_received_packet(packet: bytes, adapter: MockNetworkAdapter):
+    packet = Ether(packet)
+    assert packet.layers() == [Ether, IP]
+
+    ether = packet.getlayer(Ether)
+    assert ether.src == TEST_DST_MAC
+    assert ether.dst == adapter.mac
+
+    ip = packet.getlayer(IP)
+    assert ip.src == TEST_DST_IP
+    assert ip.dst == adapter.ip
+
 @pytest.mark.asyncio
 async def test_send(adapter: MockNetworkAdapter):
     await stack.send(IPv4, adapter, dst_ip=TEST_DST_IP, protocol=123, dst_mac=TEST_DST_MAC)
     assert_packet(adapter.get_next_packet_nowait(), adapter)
+
+@pytest.mark.asyncio
+async def test_handle(adapter: MockNetworkAdapter):
+    ip = IP(src=TEST_DST_IP, dst=adapter.ip, proto=TEST_PREVIOUS_ID)
+    packet = ip / TEST_PAYLOAD
+    description = {}
+    rest_of_packet, prev_id = await IPv4().handle(packet.build(), adapter, description)
+    assert description['src_ip'] == TEST_DST_IP
+    assert description['dst_ip'] == adapter.ip
+    assert rest_of_packet == TEST_PAYLOAD
+    assert prev_id == TEST_PREVIOUS_ID
