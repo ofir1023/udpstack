4c0a846a8048ab1a7bb6adc9827f44588e0870df
Jacob Eiderman <jacobeid@gmail.com>
2021-12-02 11:48:57 +0200

    Add UDPSocket

---
+++ test/test_udp_socket.py
@@ -0,0 +1,105 @@
+from scapy.all import Ether, IP, Padding
+from scapy.all import UDP as SCAPY_UDP
+import pytest
+import asyncio
+
+from stack import stack, NetworkAdapterInterface
+from udp import UDP
+from ethernet import Ethernet, MacResolverInterface
+from udp_socket import UDPSocket
+from network_adapter import MockNetworkAdapter
+from ip_utils import IPAddress
+
+
+TEST_DST_IP = IPAddress('1.1.1.1')
+TEST_DST_MAC = 'aa:aa:aa:aa:aa:aa'
+TEST_PREVIOUS_ID = 123
+TEST_PAYLOAD = b'abcde'
+TEST_SRC_PORT = 1234
+TEST_DST_PORT = 1337
+
+
+class MockMacResolver(MacResolverInterface):
+    async def get_mac(self, adapter: NetworkAdapterInterface, dst_ip: IPAddress) -> str:
+        assert dst_ip == TEST_DST_IP
+        return TEST_DST_MAC
+
+def assert_packet(packet: bytes, adapter: MockNetworkAdapter):
+    packet = Ether(packet)
+    assert packet.layers() == [Ether, IP, SCAPY_UDP, Padding]
+
+    ether = packet.getlayer(Ether)
+    assert ether.src == adapter.mac
+    assert ether.dst == TEST_DST_MAC
+
+    ip = packet.getlayer(IP)
+    assert ip.src == adapter.ip
+    assert ip.dst == TEST_DST_IP
+
+    udp = packet.getlayer(SCAPY_UDP)
+    assert udp.sport == TEST_SRC_PORT
+    assert udp.dport == TEST_DST_PORT
+    assert udp.payload.load == TEST_PAYLOAD
+
+
+@pytest.mark.asyncio
+async def test_send(adapter: MockNetworkAdapter):
+    stack.get_protocol(Ethernet).set_mac_resolver(MockMacResolver())
+    s = UDPSocket()
+    s.bind(TEST_SRC_PORT)
+    s.connect(TEST_DST_IP, TEST_DST_PORT)
+    await s.send(TEST_PAYLOAD)
+    assert_packet(adapter.get_next_packet_nowait(), adapter)
+
+
+@pytest.mark.asyncio
+async def test_handle(adapter: MockNetworkAdapter):
+    s = UDPSocket()
+    s.bind(TEST_DST_PORT)
+
+    udp = SCAPY_UDP(sport=TEST_SRC_PORT, dport=TEST_DST_PORT)
+    packet = udp / TEST_PAYLOAD
+    description = {'src_ip': adapter.ip, 'dst_ip': TEST_DST_IP}
+    await stack.get_protocol(UDP).handle(packet.build(), adapter, description)
+
+    assert await s.recv(len(TEST_PAYLOAD)) == TEST_PAYLOAD
+    s.close()
+
+@pytest.mark.asyncio
+async def test_two_binds(adapter: MockNetworkAdapter):
+    s1 = UDPSocket()
+    s1.bind(TEST_DST_PORT)
+
+    s2 = UDPSocket()
+    try:
+        s2.bind(TEST_DST_PORT)
+        assert False, "an exception should have been thrown"
+    except:
+        pass
+
+
+@pytest.mark.asyncio
+async def test_send_without_connect(adapter: MockNetworkAdapter):
+    s = UDPSocket()
+    try:
+        s.send(TEST_PAYLOAD)
+        assert False, "an exception should have been thrown"
+    except:
+        pass
+
+@pytest.mark.asyncio
+async def test_send_without_bind(adapter: MockNetworkAdapter):
+    s = UDPSocket()
+    s.connect(TEST_DST_IP, TEST_DST_PORT)
+    s.send(TEST_PAYLOAD)
+    s.close()
+    assert s.src_port is not None, "socket should be bound now"
+
+@pytest.mark.asyncio
+async def test_recv_without_bind(adapter: MockNetworkAdapter):
+    s = UDPSocket()
+    try:
+        s.recv(len(TEST_PAYLOAD))
+        assert False, "an exception should have been thrown"
+    except:
+        pass
---
+++ udp_socket.py
@@ -0,0 +1,57 @@
+import random
+from stack import stack
+from udp import UDP, PacketQueue
+
+class UDPSocket:
+    def __init__(self):
+        self.src_port = None
+        self.dst_ip = None
+        self.dst_port = None
+
+    def bind(self, src_port: int):
+        if src_port < 0 or src_port > 65535:
+            raise Exception("trying to bind to invalid port number")
+
+        if src_port == 0:
+            src_port = random.randint(1, 65535)
+
+        if not stack.get_protocol(UDP).create_queue(src_port):
+            raise Exception("there's already a bound socket on port {}".format(src_port))
+
+        self.src_port = src_port
+
+    def connect(self, dst_ip: str, dst_port: int):
+        self.dst_ip = dst_ip
+        self.dst_port = dst_port
+
+    async def send(self, data):
+        if self.dst_port is None or self.dst_ip is None:
+            raise Exception("cannot send on an unconnected socket")
+
+        if self.src_port is None:
+            self.bind(0)
+
+        await stack.send(UDP, src_port=self.src_port, dst_port=self.dst_port, dst_ip=self.dst_ip, data=data)
+
+    async def sendto(self, data, dst_ip: str, dst_port: int):
+        if self.src_port is None:
+            self.bind(0)
+            
+        return stack.send(UDP, src_port=self.src_port, dst_port=dst_port, dst_ip=dst_ip, data=data)
+    
+    async def recv(self, buffer_size):
+        packet = await self.recvfrom(buffer_size)
+        return packet[2]
+
+    async def recvfrom(self, buffer_size):
+        if self.src_port is None:
+            raise Exception("cannot receive on an unbound socket")
+
+        packet = await stack.get_protocol(UDP).get_packet(self.src_port)
+        packet = (packet[0], packet[1], packet[2][:buffer_size])
+        return packet
+
+    def close(self):
+        stack.get_protocol(UDP).destroy_queue(self.src_port)
+    
+
